9fe4f8f45e93924d5d9461f87be6a10d8df078bf
Andrei <bojieitsit@icloud.com>
2023-11-11 00:02:45 +0400

    Initial Commit

---
+++ SurfAPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfAPlayer
+//
+//  Created by Andrei Bogoslovskii on 11.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
