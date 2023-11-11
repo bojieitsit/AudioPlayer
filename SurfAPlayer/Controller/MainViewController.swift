17b8c1f5b821bf32f166e44efe5e8b4ea5e970bb
Andrei <bojieitsit@icloud.com>
2023-11-11 20:56:41 +0400

    Completed

--- SurfAPlayer/Controller/MainViewController.swift
+++ SurfAPlayer/Controller/MainViewController.swift
@@ -12,36 +12,44 @@ class MainViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var songs: [String] = []
+    var songIndex: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         tableView.delegate = self
         tableView.dataSource = self
-        
         songs = fetchSongs() ?? []
     }
+    
+    //MARK: - Life cycle
     
     func fetchSongs() -> [String]? {
         guard let assetPaths = Bundle.main.urls(forResourcesWithExtension: "mp3", subdirectory: nil) else {
             return nil
         }
-        
         return assetPaths.map { $0.deletingPathExtension().lastPathComponent }
     }
+    
+    func getIndexForSong(named songTitle: String) {
+        songIndex = songs.firstIndex(of: songTitle) ?? 0
+    }
+    
+    //MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToPlayer" {
             if let indexPath = tableView.indexPathForSelectedRow {
                 let playerVC = segue.destination as! PlayerViewController
                 playerVC.songTitle = songs[indexPath.row]
-                playerVC.artist = "Unknown Artist"  // set your artist here
+                playerVC.artist = "Unknown Artist"
+                playerVC.songs = songs
+                playerVC.currentIndex = indexPath.row
             }
         }
     }
-    
 }
 
+//MARK: - UITableView Delegate & DataSource
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     
@@ -52,14 +60,13 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "songCell", for: indexPath)
         let songName = songs[indexPath.row]
-        
         cell.textLabel?.text = songName
-        
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.performSegue(withIdentifier: "goToPlayer", sender: self)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 }
--- SurfAPlayer/Controller/PlayerViewController.swift
+++ SurfAPlayer/Controller/PlayerViewController.swift
@@ -12,6 +12,7 @@ import AVFoundation
 
 class PlayerViewController: UIViewController {
     
+    //MARK: - IBOutlets
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
@@ -20,59 +21,89 @@ class PlayerViewController: UIViewController {
     @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var playPauseButton: UIButton!
     
+    //MARK: - Properties
     var songTitle: String!
     var artist: String!
     var player: AVAudioPlayer!
     var timer: Timer!
+    var currentIndex: Int?
+    var songs: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
         nameLabel.text = songTitle
         authorLabel.text = artist
         configPlayer()
         setupTimer()
         player.play()
-     }
+    }
     
-    func configPlayer() {
-            if let url = Bundle.main.url(forResource: songTitle, withExtension: "mp3") {
-                do {
-                    player = try AVAudioPlayer(contentsOf: url)
-                    fullTimeLabel.text = getTimeString(from: player.duration)
-                    progressView.progress = 0.0
-                } catch {
-                    print("Cannot play this song.")
-                }
-            }
-        }
-        
-        func setupTimer() {
-            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-                if self.player.isPlaying {
-                    self.progressView.progress = Float(self.player.currentTime / self.player.duration)
-                    self.currentTimeLabel.text = self.getTimeString(from: self.player.currentTime)
-                }
-            }
-        }
-
-        func getTimeString(from time: TimeInterval) -> String {
-            let minutes = Int(time)/60 % 60
-            let seconds = Int(time) % 60
-            return String(format:"%02i:%02i", minutes, seconds)
-        }
-    
-    
-    
-    @IBAction func closeButtonPressed(_ sender: UIButton) {
-        dismiss(animated: true)
+    override func viewDidDisappear(_ animated: Bool) {
         player.stop()
     }
     
-
-    @IBAction func backButtonPressed(_ sender: UIButton) {
+    //MARK: - Alert settings
+    
+    func showAlert(with message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
         
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
+    
+    //MARK: - Life cycle
+    
+    func configPlayer() {
+        guard let currentIndex = currentIndex, currentIndex < songs.count else { return }
+        let songTitle = songs[currentIndex]
+        
+        guard let url = Bundle.main.url(forResource: songTitle, withExtension: "mp3") else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            fullTimeLabel.text = getTimeString(from: player.duration)
+            progressView.progress = 0.0
+        } catch {
+            showAlert(with: "Cannot play this song")
+        }
+        nameLabel.text = songTitle
+        authorLabel.text = artist
+        
+    }
+    
+    func setupTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            if self.player.isPlaying {
+                self.progressView.progress = Float(self.player.currentTime / self.player.duration)
+                self.currentTimeLabel.text = self.getTimeString(from: self.player.currentTime)
+            }
+        }
+    }
+    
+    func getTimeString(from time: TimeInterval) -> String {
+        let minutes = Int(time)/60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i", minutes, seconds)
+    }
+    
+    //MARK: - Navigation
+    
+    @IBAction func closeButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+    //MARK: - IBActions
+    
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        if let currentIndex = currentIndex, currentIndex > 0 {
+            self.currentIndex = currentIndex - 1
+            configPlayer()
+            player.play()
+        } else {
+            showAlert(with: "This is the first song, cannot move backward")
+        }
     }
     
     @IBAction func playButtonPressed(_ sender: UIButton) {
@@ -86,9 +117,12 @@ class PlayerViewController: UIViewController {
     }
     
     @IBAction func nextButtonPressed(_ sender: UIButton) {
-        
+        if let currentIndex = currentIndex, currentIndex < songs.count - 1 {
+            self.currentIndex = currentIndex + 1
+            configPlayer()
+            player.play()
+        } else {
+            showAlert(with: "This is the last song, cannot move forward")
+        }
     }
-    
-    
-    
 }
